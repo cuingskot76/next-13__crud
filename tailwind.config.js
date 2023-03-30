9f28284d12a07923d92dc0df43af9e42a55397d0
cuingskot76 <afrizapamujo@gmail.com>
2023-03-30 23:29:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // with src folder
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
